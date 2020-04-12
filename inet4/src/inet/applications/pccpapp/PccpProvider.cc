c3c802f6cbe48105e046b4365a3060307a0b6eb9
Kilotron <cqycczq@126.com>
2020-04-12 22:15:45 +0800

    完成PccpProvider部分

---
+++ inet4/src/inet/applications/pccpapp/PccpProvider.cc
@@ -0,0 +1,114 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "PccpProvider.h"
+
+namespace inet {
+namespace pccp {
+
+Define_Module(PccpProvider);
+
+void PccpProvider::initialize(int stage)
+{
+    ApplicationBase::initialize(stage);
+    if(stage == INITSTAGE_LOCAL){
+        localPort = par("port").intValue();
+        pktLen = par("pktLen").intValue();
+        pktNum = par("pktNum").intValue();
+        startTime = par("startTime").doubleValue();
+
+        start = new cMessage("start");
+        nodeIndex = getParentModule()->getIndex();
+        std::array<uint64_t, 2> hashValue;
+        MurmurHash3_x64_128(&nodeIndex, sizeof(int), 0, hashValue.data());
+        nid.setNID(hashValue);
+        nid.test = nodeIndex;
+    }
+}
+
+void PccpProvider::handleMessageWhenUp(cMessage *msg){
+    if (msg->isSelfMessage())
+        handleSelfMessage(msg);
+    else {
+        currentSocket->processMessage(msg);
+    }
+    if (operationalState == State::STOPPING_OPERATION )
+        startActiveOperationExtraTimeOrFinish(par("stopOperationExtraTime"));
+}
+
+void PccpProvider::handleSelfMessage(cMessage *msg)
+{
+    if(msg == start) {
+        while(content < pktNum) {
+            generateAndCacheData({nodeIndex, content});
+            content++;
+        }
+    }
+}
+
+void PccpProvider::finish()
+{
+    ApplicationBase::finish();
+}
+
+void PccpProvider::refreshDisplay() const
+{
+    ApplicationBase::refreshDisplay();
+}
+
+void PccpProvider::generateAndCacheData(const SID &sid){
+
+    Packet* pkt = new Packet("data");
+    auto payload = makeShared<ByteCountChunk>(B(pktLen));
+    pkt->insertAtBack(payload);
+    currentSocket->CacheData(sid,pkt);
+}
+
+bool PccpProvider::isEnabled()
+{
+    return true;
+}
+
+void PccpProvider::handleStartOperation(LifecycleOperation *operation)
+{
+    if (isEnabled()) {
+        currentSocket = new ColorSocket(&Protocol::color, gate("socketOut"));
+        currentSocket->bind(&Protocol::color,nid, localPort);
+        currentSocket->setCallback(this);
+        scheduleAt(startTime, start);
+    }
+}
+
+void PccpProvider::handleStopOperation(LifecycleOperation *operation)
+{
+}
+
+void PccpProvider::handleCrashOperation(LifecycleOperation *operation)
+{
+}
+
+void PccpProvider::socketDataArrived(ColorSocket *socket, Packet *packet)
+{
+    // will never arrive
+}
+
+void PccpProvider::socketClosed(ColorSocket *socket)
+{
+   delete currentSocket;
+   currentSocket = nullptr;
+}
+
+} // namespace pccp
+} //namespace inet
---
+++ inet4/src/inet/applications/pccpapp/PccpProvider.h
@@ -0,0 +1,78 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __INET_MODIFICATION_PCCPPROVIDER_H_
+#define __INET_MODIFICATION_PCCPPROVIDER_H_
+
+#include "inet/common/INETDefs.h"
+#include "inet/applications/base/ApplicationBase.h"
+#include "inet/common/lifecycle/LifecycleOperation.h"
+#include "inet/common/lifecycle/NodeStatus.h"
+#include "inet/networklayer/contract/color/ColorSocket.h"
+#include "inet/networklayer/icn/field/SID.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/common/packet/chunk/ByteCountChunk.h"
+
+namespace inet {
+namespace pccp {
+
+class INET_API PccpProvider : public ApplicationBase, public ColorSocket::ICallback
+{
+    private:
+        // parameters
+        int localPort; // not used now
+        int pktLen;
+        int pktNum;
+        simtime_t startTime;
+
+        int nodeIndex;
+        NID nid;
+
+        ColorSocket *currentSocket = nullptr;
+
+        cMessage *start = nullptr;
+        long long content = 0;
+
+    public:
+
+        PccpProvider(){};
+        ~PccpProvider(){};
+
+        virtual void initialize(int stage) override;
+        virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+        virtual void handleMessageWhenUp(cMessage *msg) override;
+        virtual void handleSelfMessage(cMessage *msg);
+        virtual void finish() override;
+        virtual void refreshDisplay() const override;
+
+        void generateAndCacheData(const SID &sid);
+
+        bool isEnabled();
+
+        // Lifecycle methods
+        virtual void handleStartOperation(LifecycleOperation *operation) override;
+        virtual void handleStopOperation(LifecycleOperation *operation) override;
+        virtual void handleCrashOperation(LifecycleOperation *operation) override;
+
+        //INetworkSocket::ICallback:
+        virtual void socketDataArrived(ColorSocket *socket, Packet *packet) override;
+        virtual void socketClosed(ColorSocket *socket) override;
+
+};
+
+} // namespace pccp
+} // namespace inet
+
+#endif
